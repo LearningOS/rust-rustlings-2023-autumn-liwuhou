a610cce58243c2da854d1945b9132e6e3b1f04d0
williamli <williamli@xiaoe-tech.com>
2024-02-22 18:56:57 +0800

    update

--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -9,7 +9,7 @@
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0.clone());
+    let mut vec1 = fill_vec(vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -23,5 +23,5 @@ fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(44);
     vec.push(66);
 
-    vec.clone()
+    vec
 }
