7ad87e429c9915e5b2a639c17ae3b19d10f5d6cf
liwuhou <hugewilliam@foxmail.com>
2024-02-23 23:04:26 +0800

    update

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -7,19 +7,17 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &'static str = "Cucumber";
         pub const CARROT: &'static str = "Carrot";
     }
@@ -30,5 +28,12 @@ fn main() {
         "favorite snacks: {} and {}",
         delicious_snacks::fruit,
         delicious_snacks::veggie
+    );
+    println!(
+        "{} - {}, {} - {}",
+        delicious_snacks::fruits::PEAR,
+        delicious_snacks::fruits::APPLE,
+        delicious_snacks::veggies::CUCUMBER,
+        delicious_snacks::veggies::CARROT
     );
 }
